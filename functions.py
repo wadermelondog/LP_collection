43e16a0aff6845b9813e657d889f8e51656b734d
Lauri Turunen <ag3154@student.jamk.fi>
2024-09-23 23:18:15 +0300

    Luotu drafti menu funktiosta ja pyöritetty mainissa csv:n testausta

---
+++ functions.py
@@ -0,0 +1,5 @@
+def menu():
+    print(f"Welcome to your offline vinyl collection, please choose a function.")
+    print(f"1. Search \n 2. New addition")
+    choice = int(input("Choice: "))
+    return choice
