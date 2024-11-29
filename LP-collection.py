b32f74da239c834ba4257694de22195c9df81f95
Lauri <ag3154@student.jamk.fi>
2024-11-29 11:11:22 +0200

    format hässäkän error handlingiä

--- LP-collection.py
+++ LP-collection.py
@@ -77,11 +77,14 @@ def add_record(collection) -> list:
                         elif key == "Collection Media Condition" or key == "Collection Sleeve Condition":
                             while True:
                                 try: 
-                                    print("Choose {key}")
+                                    print(f"Choose {key}")
                                     print("Choose from the following options:")
                                     print("1. M, 2. NM, 3. VG+, 4. VG, 5. G+, 6. G, 7. F, 8. P")
                                     conditions = {1: "M", 2: "NM", 3: "VG+", 4: "VG", 5: "G+", 6: "G", 7: "F", 8: "P"}
                                     condition = input("Enter the choice: ")
+                                    if condition not in conditions:
+                                        print("Invalid choice, try again.")
+                                        continue
                                     record[key] = conditions[int(condition)]
                                     break
                                 except ValueError:
@@ -93,22 +96,21 @@ def add_record(collection) -> list:
                                     print("Choose from the following options:")
                                     print("1. LP, 2. 2xLP, 3. 3xLP, 4. 7\", 5. 10\", 6. 12\"")
                                     formats = {1: "LP", 2: "2xLP", 3: "3xLP", 4: "7\"", 5: "10\"", 6: "12\""}
-                                    format = input("Enter the choice: ")
-                                    print("Format chosen:", formats[int(format)])
+                                    selected_format = input("Enter the choice: ")
+                                    
+                                    print("Format chosen:", formats[int(selected_format)])
                                     print("Is it a reissue, compilation or 180g etc?")
                                     choice = input("Yes or No: ")
-                                    if format == "1" or format == "2" or format == "3":
+                                    if selected_format == "1" or selected_format == "2" or selected_format == "3":
                                         if choice.lower() == "yes" or choice.lower() == "y":
-                                            record[key] = f"{formats[int(format)]}, Album, {input('Enter the additional information: ')}"
-                                            break
+                                            record[key] = f"{formats[int(selected_format)]}, Album, {input('Enter the additional information: ')}"
                                         elif choice.lower() == "no" or choice.lower() == "n":
-                                            record[key] = formats[int(format)]
-                                            break
+                                            record[key] = formats[int(selected_format)]
                                         else:
                                             print("Invalid choice, please try again")
                                             continue  
                                     else:
-                                        print("Format chosen:", formats[int(format)])
+                                        print("Format chosen:", formats[int(selected_format)])
                                     break
                                 except ValueError:
                                     print("Invalid choice, please try again")
@@ -116,7 +118,7 @@ def add_record(collection) -> list:
                         else: 
                             user_input = input(f"{key}: ")
                             record[key] = user_input
-                            break
+                        break
                     except ValueError:
                         print(f"Invalid input for {key}. Expected {input_types[key].__name__}.")
         collection.append(record)
